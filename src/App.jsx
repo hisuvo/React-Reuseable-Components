e8bd6431343b5e7abd9d393d92f8bc6c10c86a71
suvo <suvodatta72@gmail.com>
2024-11-08 09:39:16 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-2xl font-semibold'>Lorem ipsum dolor sit amet.</h2>
+    <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nesciunt saepe qui repellendus rerum assumenda deserunt, voluptatem recusandae. Eveniet exercitationem libero modi id, unde amet soluta repudiandae fugiat ea dicta perferendis harum non corporis aliquam dolorem odit, rerum, saepe laboriosam rem reiciendis? Pariatur, quaerat dolore.</p>
+    <button className='btn'>Click Me!</button>
+    </>
+  )
+}
+
+export default App
